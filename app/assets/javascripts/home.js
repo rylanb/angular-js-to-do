cf8df5b4f83940bd281d4527250bc791a0919e04
Rylan Bowers <rylanb@gmail.com>
2012-07-11 13:18:43 -0700

    Adding in basic style for to-do app and making Angular call manually.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+//Angular Functions
 function ToDosListCTRL($scope) {
   $scope.todos = [
     {"title": "Buy lots of things"},
@@ -8,3 +9,21 @@ function ToDosListCTRL($scope) {
     {"title": "Testing"}
   ];
 }
+
+//Setup for binding of form/delete events
+$(function() {
+  //Call angular setup/functions to prevent auto creation
+  angular.element(document).ready(function() {
+    angular.bootstrap(document);
+  });
+
+  //Bind Click/Submit Handlers
+  $('#todos_form').submit(function(){
+    $.ajax({
+      url: "/todos/new",
+      context: document.body
+    }).done(function() {
+      $(this).addClass("done");
+    });
+  })
+});
