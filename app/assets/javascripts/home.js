19cab9f80e5b14a929017634dfcca243dd8acbb4
Rylan Bowers <rylanb@gmail.com>
2012-07-11 12:04:25 -0700

    Getting Angular to work with dummy data.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function ToDosListCTRL($scope) {
+  $scope.todos = [
+    {"title": "Buy lots of things"},
+    {"title": "Sell It Out!"},
+    {"title": "Testing"}
+  ];
+}
