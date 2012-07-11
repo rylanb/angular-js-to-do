e37ddd86017114b9fdd6da64b9ae8b7283d87351
Rylan Bowers <rylanb@gmail.com>
2012-07-11 14:53:05 -0700

    Home controller and tests.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @todos = Todo.all
   end
 
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe HomeController do
 
+  context "#index" do
+    before do
+      get :index
+    end
+    it { should respond_with(:success) }
+    it { should render_template(:index) }
+    it { should assign_to(:todos) }
+  end
+
 end
