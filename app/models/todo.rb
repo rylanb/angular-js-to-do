ce92065f53453bd4a94e9babe50c28cf8a457baf
Rylan Bowers <rylanb@gmail.com>
2012-07-11 11:24:35 -0700

    Adding done column to dos and testing the model.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,8 @@
 class Todo < ActiveRecord::Base
   attr_accessible :title
+  validates_presence_of :title
+
+  def finished?
+    return self.done == true
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120711174746) do
+ActiveRecord::Schema.define(:version => 20120711181725) do
 
   create_table "todos", :force => true do |t|
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "done"
   end
 
 end
