980a4a5e0b1ef5622fa16d926083b8bc000ac6af
Rylan Bowers <rylanb@gmail.com>
2012-07-11 14:51:39 -0700

    Moving todo specs into todo_spec.rb.

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
-describe Todo do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ToDo do
+  it "has a valid factory" do
+    FactoryGirl.create(:todo).should be_valid
+  end
+
+  it "should not be valid without a title" do
+    FactoryGirl.build(:todo, title: nil).should_not be_valid
+  end
+
+  it "should mark new to-do as unfinished" do
+    todo = FactoryGirl.build(:todo)
+    todo.finished?.should eq(false)
+  end
+
+  it "should return done to-do as finished" do
+    todo = FactoryGirl.build(:done)
+    todo.finished?.should eq(true)
+  end
+
 end
